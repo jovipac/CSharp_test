c319f13701bd96c207d4d402843536a9c556ab08
jovipac <jovipac@hotmail.com>
2019-03-18 20:06:46 -0600

    Iniciacion del proyecto
    Creacion del formulario de logueo y conexion a la Base de datos

---
+++ ReservaAereas/frmLogin.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ReservaAereas
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(165, 53);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(165, 101);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(93, 53);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(43, 13);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Usuario";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(93, 104);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(34, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Clave";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(283, 166);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Iniciar";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 246);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ ReservaAereas/frmLogin.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReservaAereas
+{
+    public partial class frmLogin : Form
+    {
+        /// <summary>
+        /// Key for the crypto provider
+        /// </summary>
+        private static readonly byte[] _key = { 0xA1, 0xF1, 0xA6, 0xBB, 0xA2, 0x5A, 0x37, 0x6F, 0x81, 0x2E, 0x17, 0x41, 0x72, 0x2C, 0x43, 0x27 };
+        /// <summary>
+        /// Initialization vector for the crypto provider
+        /// </summary>
+        private static readonly byte[] _initVector = { 0xE1, 0xF1, 0xA6, 0xBB, 0xA9, 0x5B, 0x31, 0x2F, 0x81, 0x2E, 0x17, 0x4C, 0xA2, 0x81, 0x53, 0x61 };
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+#if (DEBUG) //Only compile this method for local debugging.
+        /// <summary>
+        /// Decrypt a string
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        private static string Decrypt(string Value)
+        {
+            SymmetricAlgorithm mCSP;
+            ICryptoTransform ct = null;
+            MemoryStream ms = null;
+            CryptoStream cs = null;
+            byte[] byt;
+            byte[] _result;
+            mCSP = new RijndaelManaged();
+            try
+            {
+                mCSP.Key = _key;
+                mCSP.IV = _initVector;
+                ct = mCSP.CreateDecryptor(mCSP.Key, mCSP.IV);
+                byt = Convert.FromBase64String(Value);
+                ms = new MemoryStream();
+                cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+                cs.Write(byt, 0, byt.Length);
+                cs.FlushFinalBlock();
+                cs.Close();
+                _result = ms.ToArray();
+            }
+            catch
+            {
+                _result = null;
+            }
+            finally
+            {
+                if (ct != null)
+                    ct.Dispose();
+                if (ms != null)
+                    ms.Dispose();
+                if (cs != null)
+                    cs.Dispose();
+            }
+            return ASCIIEncoding.UTF8.GetString(_result);
+        }
+#endif
+        /// <summary>
+        /// Encrypt a string
+        /// </summary>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        private static string Encrypt(string Password)
+        {
+            if (string.IsNullOrEmpty(Password))
+                return string.Empty;
+            byte[] Value = Encoding.UTF8.GetBytes(Password);
+            SymmetricAlgorithm mCSP = new RijndaelManaged();
+            mCSP.Key = _key;
+            mCSP.IV = _initVector;
+            using (ICryptoTransform ct = mCSP.CreateEncryptor(mCSP.Key, mCSP.IV))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, ct, CryptoStreamMode.Write))
+                    {
+                        cs.Write(Value, 0, Value.Length);
+                        cs.FlushFinalBlock();
+                        cs.Close();
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Looks up the users password crypto string in the database
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <returns></returns>
+        private static DataTable LookupUser(string Username)
+        {
+            /*
+             * The reason I return a datatable here is so you can also bring back the user's full
+             * name, email address, security rights in the application, etc. I have a "User" class
+             * where I defined meta information for users.
+             */
+            const string connStr = "Data Source=802PACAJOJ\\SQLEXPRESS;Initial Catalog=flujoaereo;Integrated Security=True;";
+            const string query = "Select Password From Users (NOLOCK) Where UserName = @UserName";
+            DataTable result = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.Add("@UserName", SqlDbType.VarChar).Value = Username;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        result.Load(dr);
+                    }
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Obviously the .Focus() code doesn't apply to ASP.NET
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxUsername.Text))
+            {
+                //Focus box before showing a message
+                textBoxUsername.Focus();
+                MessageBox.Show("Enter your username", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Focus again afterwards, sometimes people double click message boxes and select another control accidentally
+                textBoxUsername.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(textBoxPassword.Text))
+            {
+                textBoxPassword.Focus();
+                MessageBox.Show("Enter your password", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBoxPassword.Focus();
+                return;
+            }
+            //OK they enter a user and pass, lets see if they can authenticate
+            using (DataTable dt = LookupUser(textBoxUsername.Text))
+            {
+                if (dt.Rows.Count == 0)
+                {
+                    textBoxUsername.Focus();
+                    MessageBox.Show("Invalid username.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBoxUsername.Focus();
+                    return;
+                }
+                else
+                {
+                    //Always compare the resulting crypto string or hash value, never the decrypted value
+                    //By doing that you never make a call to Decrypt() and the application is harder to
+                    //reverse engineer. I included the Decrypt() method here for informational purposes
+                    //only. I do not recommend shipping an assembly with Decrypt() methods.
+                    string dbPassword = Convert.ToString(dt.Rows[0]["Password"]).Trim();
+                    string appPassword = Encrypt(textBoxPassword.Text); //we store the password as encrypted in the DB
+                    if (string.Compare(dbPassword, appPassword) == 0)
+                    {
+                        //frmLogin.ActiveForm.Close();
+                    }
+                    else
+                    {
+                        //You may want to use the same error message so they can't tell which field they got wrong
+                        textBoxPassword.Focus();
+                        MessageBox.Show("Invalid Password", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        textBoxPassword.Focus();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
